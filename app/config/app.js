8e22e45d565e71ad13c01a60653d03accab956bc
Vishesh Shalak <vishesh.pbl@gmail.com>
2016-07-17 20:02:27 +0530

    adding proxy server for cors

--- app/config/app.js
+++ app/config/app.js
@@ -1,7 +1,11 @@
 var app = angular.module('app',[])
 
 
-    .config(function($locationProvider){
+    .config(function($locationProvider,$httpProvider){
+
+        //Enable cross domain calls
+        $httpProvider.defaults.useXDomain = true;
+
 
         $locationProvider.html5Mode(true);
         //console.log("appInitialized");
@@ -9,7 +13,7 @@ var app = angular.module('app',[])
     })
 
     .value('distance', 1000 )                                                          // Radius of the area in which instagram will search images in meters
-    .value('baseUrlInsta','https://api.instagram.com/v1/')                             // base url instagram api
+    .value('baseUrlInsta','https://crossorigin.me/https://api.instagram.com/v1/')      // base url instagram api with a proxy server
     .value('accessTokenInsta','3526363720.6d51b61.d94d294e80454f71b0d47f64fe5ff69f')   // instagram access token
     .value('accessTokenGoogle' , 'AIzaSyCGxc1R2iqgqbH8aChInIz98nfvW9ZO0WI');           // google map javascript api access token
 
